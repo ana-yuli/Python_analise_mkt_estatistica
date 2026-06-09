34798ec6ad3855e51a3e6f688fa903bc98e5fa80
Ana Yuli <ana.yuli@hotmail.com>
2026-06-09 17:50:39 -0300

    update

--- data_mkt - cópia.py
+++ data_mkt - cópia.py
@@ -39,3 +39,51 @@ sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', center=0)
 plt.title('Correlation Matrix of Numerical Variables')
 plt.tight_layout()
 plt.show()
+
+# Conclusão do gráfico: não existe relação entre as variáveis qualitativas
+
+# %% Limpeza de dados e cálculos
+
+
+# Transformar a variável'Acquisition_Cost' em números, pois seu tipo está em object:
+# Primeiro, remover os textos "$",vírgula e espaços em branco
+
+dataset_mkt['Acquisition_Cost'] = dataset_mkt['Acquisition_Cost'].str.replace('[/$,]', '', regex=True).str.strip()
+# regex=True - Regex significa Regular Expression (Expressão Regular), é uma forma de procurar padrões em texto
+# .strip() - Remove os espaços em branco após o texto
+
+# Segundo, conversão de object para número:
+
+dataset_mkt['Acquisition_Cost'] = pd.to_numeric(dataset_mkt['Acquisition_Cost'], errors='coerce')
+# errors='coerce' - transforma valores inválidos em NaN, SUPER IMPORTANTE O COERCE PARA NAO DAR ERRO NOS DADOS
+
+
+# Cálculo ce CTR e CPC
+
+# CTR = Clicks / Impressões
+dataset_mkt['CTR'] = dataset_mkt['Clicks'] / dataset_mkt['Impressions']
+
+# CPC = Custo de aquisição / Clicks
+
+dataset_mkt['CPC'] = dataset_mkt['Acquisition_Cost'] / dataset_mkt['Clicks']
+
+# %% Gráfico de distribuição CTR e CPC
+
+# Gráfico CTR
+plt.figure(figsize=(20, 10))
+plt.subplot(1, 2, 1)
+sns.histplot(dataset_mkt['CTR'].dropna(), kde=True, bins=25, color='Green')
+plt.title('Distribuição CTR')
+plt.xlabel('CTR')
+plt.ylabel('Frequência')
+
+# Gráfico CPC
+plt.subplot(1, 2, 2)
+sns.histplot(dataset_mkt['CPC'].dropna(), kde=True, bins=25, color='Blue')
+plt.title('Distribuição CPC')
+plt.xlabel('CPC')
+plt.ylabel('frequência')
+
+plt.tight_layout()
+plt.show()
+
